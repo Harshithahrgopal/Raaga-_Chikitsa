f8a35468039e81a75e2aa33c111ce4c62fd4a5fc
Harshithahrgopal <harshithahg.cs23@bmsce.ac.in>
2025-05-08 16:00:54 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,198 @@
+      // Figma Flutter Generator Iphone13146Widget - FRAME
+      Container(
+      width: 390,
+      height: 844,
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(15),
+            topRight: Radius.circular(15),
+            bottomLeft: Radius.circular(15),
+            bottomRight: Radius.circular(15),
+          ),
+      gradient : LinearGradient(
+          begin: Alignment(6.123234262925839e-17,1),
+          end: Alignment(-1,6.123234262925839e-17),
+          colors: [Color.fromRGBO(255, 241, 219, 1),Color.fromRGBO(255, 188, 151, 1)]
+        ),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 91,
+        left: 87,
+        child: Text('Welcome Back!', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(92, 2, 2, 1),
+        fontFamily: 'Inter',
+        fontSize: 30,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),Positioned(
+        top: 141,
+        left: 84,
+        child: Text('Reconnect with wellness', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(106, 24, 5, 0.7900000214576721),
+        fontFamily: 'Inter',
+        fontSize: 19,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),Positioned(
+        top: 241,
+        left: 48,
+        child: Container(
+        width: 300,
+        height: 55,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(17),
+            topRight: Radius.circular(17),
+            bottomLeft: Radius.circular(17),
+            bottomRight: Radius.circular(17),
+          ),
+      color : Color.fromRGBO(255, 241, 219, 1),
+      border : Border.all(
+          color: Color.fromRGBO(106, 24, 5, 1),
+          width: 2,
+        ),
+  )
+      )
+      ),Positioned(
+        top: 367,
+        left: 48,
+        child: Container(
+        width: 300,
+        height: 55,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(17),
+            topRight: Radius.circular(17),
+            bottomLeft: Radius.circular(17),
+            bottomRight: Radius.circular(17),
+          ),
+      color : Color.fromRGBO(255, 241, 219, 1),
+      border : Border.all(
+          color: Color.fromRGBO(106, 24, 5, 1),
+          width: 2,
+        ),
+  )
+      )
+      ),Positioned(
+        top: 203,
+        left: 62,
+        child: Text('Email', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(92, 2, 2, 1),
+        fontFamily: 'Inter',
+        fontSize: 20,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),Positioned(
+        top: 327,
+        left: 62,
+        child: Text('Password', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(92, 2, 2, 1),
+        fontFamily: 'Inter',
+        fontSize: 20,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),Positioned(
+        top: 385,
+        left: 73,
+        child: Text('Enter your Password', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(106, 24, 5, 0.7900000214576721),
+        fontFamily: 'Inter',
+        fontSize: 16,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),Positioned(
+        top: 258,
+        left: 73,
+        child: Text('Enter your Email', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(106, 24, 5, 0.7900000214576721),
+        fontFamily: 'Inter',
+        fontSize: 17,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),Positioned(
+        top: 447,
+        left: 195,
+        child: Text('Forgot Password?', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(92, 2, 2, 1),
+        fontFamily: 'Inter',
+        fontSize: 18,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),Positioned(
+        top: 514,
+        left: 108,
+        child: Container(
+        width: 170,
+        height: 51,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(24),
+            topRight: Radius.circular(24),
+            bottomLeft: Radius.circular(24),
+            bottomRight: Radius.circular(24),
+          ),
+      boxShadow : [BoxShadow(
+          color: Color.fromRGBO(0, 0, 0, 0.25),
+          offset: Offset(0,4),
+          blurRadius: 4
+      )],
+      color : Color.fromRGBO(212, 141, 102, 0.5899999737739563),
+      border : Border.all(
+          color: Color.fromRGBO(106, 24, 5, 1),
+          width: 3,
+        ),
+  )
+      )
+      ),Positioned(
+        top: 523,
+        left: 151,
+        child: Text('Login', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(92, 2, 2, 1),
+        fontFamily: 'Inter',
+        fontSize: 28,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),Positioned(
+        top: 599,
+        left: 46,
+        child: Text('Don’t have an account?  ', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(92, 2, 2, 1),
+        fontFamily: 'Inter',
+        fontSize: 19,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),Positioned(
+        top: 597,
+        left: 265,
+        child: Text('Sign Up', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(5, 9, 210, 1),
+        fontFamily: 'Inter',
+        fontSize: 21,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),
+        ]
+      )
+    )
