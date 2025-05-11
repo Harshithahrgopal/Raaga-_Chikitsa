c45d728bd25a1352a3e2bb0c66ecee91e4b93cda
Harshithahrgopal <harshithahg.cs23@bmsce.ac.in>
2025-05-11 18:47:22 +0530

    Update melodylist.dart

--- melodylist.dart
+++ melodylist.dart
@@ -3,113 +3,143 @@ import 'home.dart'; // Import your home.dart file
 
 const brown = Color.fromRGBO(92, 2, 2, 1);
 const lightBrown = Color.fromRGBO(212, 141, 102, 1);
+const raagaDivColor = Color.fromRGBO(212, 141, 102, 1); // D48D66 in RGB
 const arrowBrown = Color.fromRGBO(105, 23, 5, 1);
 const borderRadiusAll = BorderRadius.all(Radius.circular(15));
 final BorderRadius itemRadius = BorderRadius.circular(12);
 const itemHeight = 44.0;
-const itemWidth = 349.0;
-const titleStyle = TextStyle(color: brown, fontFamily: 'Inter', fontSize: 30, fontWeight: FontWeight.normal, height: 1);
-const itemTextStyle = TextStyle(color: brown, fontFamily: 'Inter', fontSize: 24, fontWeight: FontWeight.normal, height: 1);
-const arrowTextStyle = TextStyle(color: arrowBrown, fontFamily: 'Inter', fontSize: 25, fontWeight: FontWeight.normal, height: 1);
+const dividerHeight = 8.0; // Increased spacing between raaga divs
+
+const titleStyle = TextStyle(
+  color: brown,
+  fontFamily: 'Inter',
+  fontSize: 30,
+  fontWeight: FontWeight.normal,
+  height: 1,
+  backgroundColor: Colors.transparent, // Make background transparent
+);
+const itemTextStyle = TextStyle(
+  color: brown,
+  fontFamily: 'Inter',
+  fontSize: 24,
+  fontWeight: FontWeight.normal,
+  height: 1,
+  backgroundColor: Colors.transparent, // Make background transparent
+);
+const arrowTextStyle = TextStyle(
+  color: arrowBrown,
+  fontFamily: 'Inter',
+  fontSize: 25,
+  fontWeight: FontWeight.normal,
+  height: 1,
+  backgroundColor: Colors.transparent, // Make background transparent
+);
 
 class MelodyList extends StatelessWidget {
   const MelodyList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 390,
-      height: 844,
-      decoration: BoxDecoration(
-        borderRadius: borderRadiusAll,
-        gradient: const LinearGradient(
-          begin: Alignment(0, 1),
-          end: Alignment(-1, 0),
-          colors: [Color(0xFFFFf1db), Color(0xFFFFbc97)],
+    return Scaffold( // Using Scaffold to properly size the content
+      body: Container(
+        width: double.infinity, // Make the container take full width
+        height: double.infinity, // Make the container take full height
+        decoration: const BoxDecoration(
+          borderRadius: borderRadiusAll,
+          gradient: LinearGradient(
+            begin: Alignment(0, 0), // Start from the top
+            end: Alignment(0, 1), // End at the bottom
+            colors: [Color(0xFFFFbc97), Color(0xFFFFf1db)], // Swapped colors
+          ),
         ),
-      ),
-      child: Stack(
-        children: <Widget>[
-          // Top Bar with Back Button and Title
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              height: 72,
-              color: lightBrown.withOpacity(0.95),
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const HomePage()), // Navigate to Home page
-                      );
-                    },
-                    child: const Text('<', style: arrowTextStyle), // Arrow before text
-                  ),
-                  const Text('Melody List', style: titleStyle),
-                  SizedBox(
-                    width: 30,
-                    height: 30,
-                    child: ClipOval(
+        child: Stack(
+          children: <Widget>[
+            // Top Bar with Back Button and Title
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: 72,
+                color: lightBrown.withOpacity(0.95),
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const HomePage()), // Navigate to Home page
+                        );
+                      },
                       child: Image.asset(
-                        'assets/ragachikitsalogo.png',
-                        fit: BoxFit.cover, // Ensure the image fills the circle
+                        'assets/arrow_left.png',
+                        width: 30,
+                        height: 30,
                       ),
                     ),
-                  ),
+                    const Text('Melody List', style: titleStyle),
+                    SizedBox(
+                      width: 30,
+                      height: 30,
+                      child: ClipOval(
+                        child: Image.asset(
+                          'assets/ragachikitsalogo.png',
+                          fit: BoxFit.cover, // Ensure the image fills the circle
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            // Melody Items
+            Positioned(
+              top: 80,
+              left: 20,
+              right: 20, // Added right padding to control width
+              child: Column(
+                children: [
+                  _buildMelodyItem('Kanakangi'),
+                  const SizedBox(height: dividerHeight),
+                  _buildMelodyItem('ratnangi'),
+                  const SizedBox(height: dividerHeight),
+                  _buildMelodyItem('Ganamurti'),
+                  const SizedBox(height: dividerHeight),
+                  _buildMelodyItem('Vanaspati'),
+                  const SizedBox(height: dividerHeight),
+                  _buildMelodyItem('Manavati'),
+                  const SizedBox(height: dividerHeight),
+                  _buildMelodyItem('Tanarupi'),
+                  const SizedBox(height: dividerHeight),
+                  _buildMelodyItem('Senavati'),
+                  const SizedBox(height: dividerHeight),
+                  _buildMelodyItem('Hanumatodi'),
+                  const SizedBox(height: dividerHeight),
+                  _buildMelodyItem('Dhenuka'),
+                  const SizedBox(height: dividerHeight),
+                  _buildMelodyItem('Natakapriya'),
+                  const SizedBox(height: dividerHeight),
+                  _buildMelodyItem('Kokilapriya'),
+                  const SizedBox(height: dividerHeight),
+                  _buildMelodyItem('Roopavathi'),
+                  const SizedBox(height: dividerHeight),
+                  _buildMelodyItem('Gayakapriya'),
                 ],
               ),
             ),
-          ),
-          // Melody Items
-          Positioned(
-            top: 80,
-            left: 20,
-            child: Column(
-              children: [
-                _buildMelodyItem('Kanakangi'),
-                const SizedBox(height: 6.0),
-                _buildMelodyItem('ratnangi'),
-                const SizedBox(height: 6.0),
-                _buildMelodyItem('Ganamurti'),
-                const SizedBox(height: 6.0),
-                _buildMelodyItem('Vanaspati'),
-                const SizedBox(height: 6.0),
-                _buildMelodyItem('Manavati'),
-                const SizedBox(height: 6.0),
-                _buildMelodyItem('Tanarupi'),
-                const SizedBox(height: 6.0),
-                _buildMelodyItem('Senavati'),
-                const SizedBox(height: 6.0),
-                _buildMelodyItem('Hanumatodi'),
-                const SizedBox(height: 6.0),
-                _buildMelodyItem('Dhenuka'),
-                const SizedBox(height: 6.0),
-                _buildMelodyItem('Natakapriya'),
-                const SizedBox(height: 6.0),
-                _buildMelodyItem('Kokilapriya'),
-                const SizedBox(height: 6.0),
-                _buildMelodyItem('Roopavathi'),
-                const SizedBox(height: 6.0),
-                _buildMelodyItem('Gayakapriya'),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 
   Widget _buildMelodyItem(String melodyName) {
     return Container(
-      width: itemWidth,
+      width: double.infinity, // Make each item take full available width
       height: itemHeight,
-      decoration: BoxDecoration(borderRadius: itemRadius, color: lightBrown),
+      decoration: BoxDecoration(borderRadius: itemRadius, color: raagaDivColor), // Set the desired color
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
